449cb5bf115f5f0ad5b7a113c06168bf312abc01
Mati01 <mati9606@gmail.com>
2017-03-06 07:59:54 -0800

    test 3 version2

--- train-system/src/main/java/hu/bme/mit/train/system/TrainTachographe.java
+++ train-system/src/main/java/hu/bme/mit/train/system/TrainTachographe.java
@@ -2,10 +2,16 @@ package hu.bme.mit.train.system;
 
 import java.sql.Time;
 
+import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.Table;
 
 public class TrainTachographe {
 
 		public Table<Time, Integer, Integer> table;
+		
+		public TrainTachographe()
+		{
+			table = HashBasedTable.create();
+		}
 		
 }
